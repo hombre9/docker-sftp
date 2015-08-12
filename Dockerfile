db7f6dafbfe53e970e763ea2a947b122bbd25d29
Markus Kosmal <code@m-ko-x.de>
2015-08-13 01:18:13 +0200

    add multi user support

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,6 @@
 FROM ubuntu:15.04
 MAINTAINER Markus Kosmal <code@m-ko-x.de>
 
-ENV USER share
-ENV PASS noway
-ENV USER_UID 1000
-
 RUN apt-get update -y && \
     apt-get install -yqq openssh-server mcrypt && \
     mkdir /var/run/sshd && chmod 0755 /var/run/sshd
@@ -12,7 +8,6 @@ RUN apt-get update -y && \
 ADD assets/bootstrap.sh /usr/local/bin/bootstrap.sh
 ADD assets/sshd_config /etc/ssh/sshd_config
 
-VOLUME ["/data"]
 EXPOSE 22
 
 ENTRYPOINT ["/bin/bash", "/usr/local/bin/bootstrap.sh"]
--- assets/bootstrap.sh
+++ assets/bootstrap.sh
@@ -1,11 +1,44 @@
 #!/bin/bash
 
-if ( id ${USER} ); then
-    echo "INFO: User ${USER} already exists"
-else
-    echo "INFO: User ${USER} does not exists, we create it"
-    ENC_PASS=$(perl -e 'print crypt($ARGV[0], "password")' ${PASS})
-    useradd -d /data -m -p ${ENC_PASS} -u ${USER_UID} -s /bin/sh ${USER}
-fi
+for users in "$@"; do
+    IFS=':' read -a data <<< "$users"
+    user="${data[0]}"
+    pass="${data[1]}"
+
+    if [ "${data[2]}" == "e" ]; then
+        chpasswdOptions="-e"
+        uid="${data[3]}"
+        gid="${data[4]}"
+    else
+        uid="${data[2]}"
+        gid="${data[3]}"
+    fi
+
+    useraddOptions="--create-home --no-user-group"
+
+    if [ -n "$uid" ]; then
+        useraddOptions="$useraddOptions --non-unique --uid $uid"
+    fi
+
+    if [ -n "$gid" ]; then
+        useraddOptions="$useraddOptions --gid $gid"
+        groupadd --gid $gid $gid
+    fi
+
+    useradd $useraddOptions $user
+    chown root:root /home/$user
+    chmod 755 /home/$user
+
+    if [ -z "$pass" ]; then
+        pass="$(echo `</dev/urandom tr -dc A-Za-z0-9 | head -c256`)"
+        chpasswdOptions=""
+    fi
+
+    echo "$user:$pass" | chpasswd $chpasswdOptions
+
+    cat /home/$user/.ssh/keys/* >> /home/$user/.ssh/authorized_keys
+    chown $user /home/$user/.ssh/authorized_keys
+    chmod 600 /home/$user/.ssh/authorized_keys
+done
 
 exec /usr/sbin/sshd -D
